e2c510e3f85c78472365c98617fde1b72637a764
aschmale4 <42650683+aschmale4@users.noreply.github.com>
2018-08-24 10:46:39 -0500

    I put my own name and major into the file.

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: austin and navya
+ * Author: Navya and Austin
  * Date: 2016/11/02
  *
  * A simple hello world program in C
@@ -10,7 +10,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("i am navya singh and my majors is computer science\n");
+  printf("I am Navya Singh and my major is Computer Science.\n");
+  printf("My name is Austin Schmale and my major is Software Engineering.\n");
 
   return 0;
 }
